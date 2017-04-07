cec5064ee6efc34c7f5215def7abf8b8c4a812c1
MroczekBartlomiej <mroczek.bartlomiej@gmail.com>
2017-04-07 21:06:51 +0200

    Task basic function

--- src/app/component/list.component.ts
+++ src/app/component/list.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit, Input} from "@angular/core";
 import "rxjs/add/operator/switchMap";
 import {Task} from "../model/task";
 import {TaskService} from "../service/task.service";
+import {STATUS} from "angular-in-memory-web-api";
 @Component({
   selector: "tasks-list",
   templateUrl: 'app/component/list.component.html',
@@ -27,11 +28,27 @@ export class ListComponent implements OnInit {
     this.taskService.getTaskByListId(listId).subscribe(_data => this.task = _data);
   }
 
-  removeTaskFromList(task: Task): void {
-
+  removeTaskFromList(taskId: string): void {
+    this.taskService.deleteTask(taskId)
+      .then(_data => {
+        if (_data.status === STATUS.OK){
+          this.getTasksForList(this.listId);}
+      });
   }
 
-  addTaskToList(): void {
+  addTaskToList(taskDescribe: string) {
+    let _newTask: Task = {
+      id: null,
+      listId: this.listId,
+      description:  taskDescribe
+    };
+    this.taskService.addTask(_newTask)
+      .then(_data => {
+        if (_data.status === STATUS.OK){
+          this.getTasksForList(this.listId);
+          this.hideNewTaskCard();
+        }
+      });
 
   }
 
@@ -41,5 +58,6 @@ export class ListComponent implements OnInit {
 
   hideNewTaskCard(): void {
     this.newTaskCardVisible = false;
+    this.newTaskDescription = null;
   }
 }
--- src/app/service/task.service.ts
+++ src/app/service/task.service.ts
@@ -1,6 +1,8 @@
 import {Injectable} from "@angular/core";
-import {Http} from "@angular/http";
+import {Http, Response, RequestMethod} from "@angular/http";
 import 'rxjs/add/operator/map'
+import {Task} from "../model/task";
+import { Headers, RequestOptions } from '@angular/http';
 
 @Injectable()
 export class TaskService {
@@ -11,10 +13,30 @@ export class TaskService {
   constructor(private http: Http) {
   }
 
+  addTask(task: Task) {
+    let headers = new Headers({ 'Content-Type': 'application/json' });
+    let options = new RequestOptions({ headers: headers,method: RequestMethod.Post });
+
+    console.log("Service add task")
+    return this.http.post(this.localhost, task, options)
+      .toPromise();
+  }
+
+  deleteTask(taskId: string){
+    const _url = `${this.localhost}/${taskId}`
+    return this.http.delete(_url)
+      .toPromise();
+  }
+
   getTaskByListId(listId: string){
     const _url = `${this.localhost}/${listId}`;
     return this.http.get(_url)
       .map(res => res.json());
+  }
+
+  private extractData(res: Response) {
+    let body = res.json();
+    return body.data || { };
   }
 
   private handleError(error: any): Promise<any> {
